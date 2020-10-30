08105797e7af41f645901271c4ef0edfd2200270
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-30 21:52:56 +0000

    finished with the currency rate assignment

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -18,4 +18,17 @@ class CurrenciesController < ApplicationController
     @from_symbol = params.fetch("from_currency")
     render({ :template => "currency_templates/step_two.html.erb"})
   end
+
+  def third_currency
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+    raw_data = open("https://api.exchangerate.host/convert?from=" + @from_symbol + "&to=" + @to_symbol).read
+    parsed_data = JSON.parse(raw_data)
+    query_hash = parsed_data.fetch("query")
+    @array_of_symbols = query_hash.keys
+    info_hash = parsed_data.fetch("info")
+    @rate = info_hash.fetch("rate")
+
+    render({ :template => "currency_templates/step_three.html.erb"})
+  end
 end
